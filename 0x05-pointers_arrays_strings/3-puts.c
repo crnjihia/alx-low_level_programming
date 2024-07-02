a5a6573ae1cb8734c63aab310c08221152da6122
Christopher Njihia <crnjihia@gmail.com>
2024-07-02 14:24:48 +0300

    Task 3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * _puts - Prints a string to stdout followed by a new line
- * @str: The string to be printed
+ * _puts - function that prints a string, followed by new line
+ * @str: takes the string character arguments
+ * Return: void
  */
+
 void _puts(char *str)
 {
-    while (*str)
-    {
-        _putchar(*str++);
-    }
-    _putchar('\n');
+	while (*str != 0)
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
-
