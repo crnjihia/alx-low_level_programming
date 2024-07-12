81ddcb25fc7c8e7602ec785e4c01d177c0dffcf6
Christopher <crnjihia@gmail.com>
2024-07-12 05:51:59 -0700

    Created _strspn.c

---
+++ 0x09-static_libraries/_strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+unsigned int _strspn(char *s, char *accept) {
+    char *p;
+    char *a;
+    unsigned int count = 0;
+    for (p = s; *p != '\0'; ++p) {
+        for (a = accept; *a != '\0'; ++a) {
+            if (*p == *a) {
+                break;
+            }
+        }
+        if (*a == '\0') {
+            return count;
+        }
+        ++count;
+    }
+    return count;
+}
+
